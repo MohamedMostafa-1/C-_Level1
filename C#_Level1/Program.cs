f1cd3235dcf9eb780beeda25afb0102cbcf73562
MohamedMostafa-1 <mohamedmostafajalal@gmail.com>
2026-08-06 20:36:37 +0300

    push Operators in DateTime

--- C#_Level1/Program.cs
+++ C#_Level1/Program.cs
@@ -153,6 +153,20 @@ namespace C_Level1
             Console.WriteLine("RESULTs: "+ result.Days);
 
 
+            //Operators in DateTime
+            DateTime dt7 = new DateTime(2015, 12, 20);
+            DateTime dt8 = new DateTime(2016, 12, 31, 5, 10, 20);
+            TimeSpan time = new TimeSpan(10, 5, 25, 50);
+
+            Console.WriteLine(dt2 + time); // 1/10/2017 10:36:10 AM
+            Console.WriteLine(dt2 - dt1); //377.05:10:20
+            Console.WriteLine(dt1 == dt2); //False
+            Console.WriteLine(dt1 != dt2); //True
+            Console.WriteLine(dt1 > dt2); //False
+            Console.WriteLine(dt1 < dt2); //True
+            Console.WriteLine(dt1 >= dt2); //False
+            Console.WriteLine(dt1 <= dt2);//True
+
             Console.ReadKey();
         }
     }
