2a0f83240f137a651d41711929a4341d2520ff1c
MohamedMostafa-1 <mohamedmostafajalal@gmail.com>
2026-08-06 19:25:27 +0300

    push Ticks

--- C#_Level1/Program.cs
+++ C#_Level1/Program.cs
@@ -111,6 +111,12 @@ namespace C_Level1
 
 
 
+            //number of 100-nanosecond intervals that have elapsed
+            //since January 1, 0001, at 00:00:00.000 in the Gregorian calendar. 
+
+            Console.WriteLine(DateTime.MinValue.Ticks);  //min value of ticks
+            Console.WriteLine(DateTime.MaxValue.Ticks); // max value of ticks
+
 
             Console.ReadKey();
         }
